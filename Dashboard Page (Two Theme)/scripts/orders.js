10080a54244353f22694e1aae888259a96426a70
HuaLocSon <hualocson@gmail.com>
2022-07-07 12:06:19 +0700

    Add Dashboard Page

---
+++ Dashboard Page (Two Theme)/scripts/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+  },
+  {
+    productName: 'Lozenge Drone',
+    productNumber: '00482',
+    paymentStatus: 'Paid',
+    shipping: 'Declined',
+  },
+  {
+    productName: 'DJI Air 2S',
+    productNumber: '81465',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+  {
+    productName: 'LAVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined',
+  },
+];
